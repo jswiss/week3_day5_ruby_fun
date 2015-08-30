9ccca48d2e0e2f2fbb9aba4d1461fdbbe71673ef
Joshua Swiss <swissjoshua@gmail.com>
2015-08-30 06:45:55 +0100

    q17 done

--- ruby_fun/questions.rb
+++ ruby_fun/questions.rb
@@ -101,7 +101,7 @@ end
 # turn an array into itself repeated twice. So [1, 2, 3]
 # becomes [1, 2, 3, 1, 2, 3]
 def double_array(array)
-  
+  array.dup + array.clone
 end
 
 # convert a symbol into a string
