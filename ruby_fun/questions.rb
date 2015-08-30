4574ce55702d948cd6d8e847566b33c501e590fc
Joshua Swiss <swissjoshua@gmail.com>
2015-08-30 07:50:24 +0100

    q21 done

--- ruby_fun/questions.rb
+++ ruby_fun/questions.rb
@@ -120,14 +120,14 @@ end
 # [1, 3, 5, 4, 1, 2, 6, 2, 1, 3, 7]
 # becomes [1, 3, 5, 4, 1, 2]
 def get_elements_until_greater_than_five(array)
-  
+
 end
 
 # turn an array (with an even number of elements) into a hash, by
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
-  
+  Hash[*array]
 end
 
 # get all the letters used in an array of words and return
@@ -135,14 +135,14 @@ end
 # . e.g. the array ['cat', 'dog', 'fish'] becomes
 # ['a', 'c', 'd', 'f', 'g', 'h', 'i', 'o', 's', 't']
 def get_all_letters_in_array_of_words(array)
-  
+  #array.map { |x| x.to_s.split('').sort }
 end
 
 # swap the keys and values in a hash. e.g.
 # {'a' => 'b', 'c' => 'd'} becomes
 # {'b' => 'a', 'd' => 'c'}
 def swap_keys_and_values_in_a_hash(hash)
-  
+  hash.invert
 end
 
 # in a hash where the keys and values are all numbers
